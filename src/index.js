261ba2781bc282d6bea7723e6ed57f742e737811
Scott Bromander <scottbromander@gmail.com>
2019-05-15 16:32:53 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import { Provider } from 'react-redux';
-import storeInstance from './module/stores/store';  
+import storeInstance from './module/stores/store';
 
 ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
